b1db4e377cb3acb3bf49bb74b0ab50c8a8c3b05a
Yoki <yokisitumorang@gmail.com>
2020-04-03 10:23:31 +0700

    Initial Commit

---
+++ DatingApp.API/Models/Value.cs
@@ -0,0 +1,12 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace DatingApp.API.Models
+{
+    public class Value
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
